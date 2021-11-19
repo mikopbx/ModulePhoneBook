171611ecd2859f0e3396192793c4244785e93887
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:24:31 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/ja/

--- Messages/ja.php
+++ Messages/ja.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => '電話帳モジュール-％repesent％',
+    'mo_ModuleModulePhoneBook' => '電話帳モジュール',
+    'BreadcrumbModulePhoneBook' => '電話帳',
+    'SubHeaderModulePhoneBook' => '通話中および会話履歴に発信者の名前を表示できます',
+    'module_phnbk_ColumnName' => '加入者の名前',
+    'module_phnbk_ColumnNumber' => '電話',
+    'module_phnbk_AddNewRecord' => '新しいエントリを追加する',
+    'module_phnbk_AlreadyExistWithThisNumber' => '番号％repesent％のエントリはすでに存在します',
+    'module_phnbk_Connected' => 'モジュールが接続されています',
+    'module_phnbk_Disconnected' => 'モジュールが無効になっています',
+];
