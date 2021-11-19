bd856a034b03932deb1b0f3adc7cfd60a286d83e
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:10:25 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/zh_Hans/

--- Messages/zh_Hans.php
+++ Messages/zh_Hans.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => '电话簿模块 -% repesent%',
+    'mo_ModuleModulePhoneBook' => '电话簿模块',
+    'BreadcrumbModulePhoneBook' => '电话簿',
+    'SubHeaderModulePhoneBook' => '允许您在通话期间和对话历史记录中显示来电者姓名',
+    'module_phnbk_ColumnName' => '订阅者姓名',
+    'module_phnbk_ColumnNumber' => '电话',
+    'module_phnbk_AddNewRecord' => '添加新条目',
+    'module_phnbk_AlreadyExistWithThisNumber' => '编号为% repesent% 的条目已存在',
+    'module_phnbk_Connected' => '模块已连接',
+    'module_phnbk_Disconnected' => '模块禁用',
+];
