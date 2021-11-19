a855486f8bb8aa9ce1e8582c3420f4a5913810a2
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:21:46 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Mô-đun danh bạ -% repesent%',
+    'mo_ModuleModulePhoneBook' => 'Mô-đun danh bạ',
+    'BreadcrumbModulePhoneBook' => 'Danh bạ điện thoại',
+    'SubHeaderModulePhoneBook' => 'Cho phép bạn hiển thị tên của người gọi trong khi gọi và trong lịch sử hội thoại',
+    'module_phnbk_ColumnName' => 'Tên người đăng ký',
+    'module_phnbk_ColumnNumber' => 'Điện thoại',
+    'module_phnbk_AddNewRecord' => 'Thêm mục nhập mới',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'Mục nhập có số% repesent% đã tồn tại',
+    'module_phnbk_Connected' => 'Mô-đun được kết nối',
+    'module_phnbk_Disconnected' => 'Mô-đun bị vô hiệu hóa',
+];
