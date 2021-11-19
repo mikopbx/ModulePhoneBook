6178e5960f58625613c7e0b69d71330039f36111
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:27:47 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'სატელეფონო წიგნის მოდული -% რეპესენტი%',
+    'mo_ModuleModulePhoneBook' => 'სატელეფონო წიგნის მოდული',
+    'BreadcrumbModulePhoneBook' => 'Სატელეფონო წიგნი',
+    'SubHeaderModulePhoneBook' => 'საშუალებას გაძლევთ აჩვენოთ აბონენტების სახელები ზარის დროს და საუბრის ისტორიაში',
+    'module_phnbk_ColumnName' => 'აბონენტის სახელი',
+    'module_phnbk_ColumnNumber' => 'ტელეფონი',
+    'module_phnbk_AddNewRecord' => 'დაამატეთ ახალი ჩანაწერი',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'ჩანაწერი ნომრით% repesent% უკვე არსებობს',
+    'module_phnbk_Connected' => 'მოდული დაკავშირებულია',
+    'module_phnbk_Disconnected' => 'მოდული გამორთულია',
+];
