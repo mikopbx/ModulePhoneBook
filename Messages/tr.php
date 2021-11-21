74e43b9d7f92b8dc3602ff7be3af57d2848e8e5d
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:26:40 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/tr/

--- Messages/tr.php
+++ Messages/tr.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Telefon rehberi modülü -% repesent%',
+    'mo_ModuleModulePhoneBook' => 'Telefon rehberi modülü',
+    'BreadcrumbModulePhoneBook' => 'Telefon rehberi',
+    'SubHeaderModulePhoneBook' => 'Bir arama sırasında ve konuşma geçmişinde arayanların adlarını görüntülemenizi sağlar',
+    'module_phnbk_ColumnName' => 'abonenin adı',
+    'module_phnbk_ColumnNumber' => 'Telefon',
+    'module_phnbk_AddNewRecord' => 'Yeni giriş ekle',
+    'module_phnbk_AlreadyExistWithThisNumber' => '% repesent% numaralı giriş zaten var',
+    'module_phnbk_Connected' => 'Modül bağlı',
+    'module_phnbk_Disconnected' => 'Modül devre dışı',
+];
