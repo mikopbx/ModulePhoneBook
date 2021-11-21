8495ffecf39738b800d88faaf86c2fc6bde21b90
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:16:40 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/pt/

--- Messages/pt.php
+++ Messages/pt.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    'module_phnbk_Disconnected' => 'Módulo desativado',
+    'module_phnbk_Connected' => 'Módulo conectado',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'A entrada com o número% repesent% já existe',
+    'module_phnbk_AddNewRecord' => 'Adicionar nova entrada',
+    'module_phnbk_ColumnNumber' => 'Telefone',
+    'module_phnbk_ColumnName' => 'Nome do assinante',
+    'SubHeaderModulePhoneBook' => 'Permite que você exiba os nomes dos chamadores durante uma chamada e no histórico de conversas',
+    'BreadcrumbModulePhoneBook' => 'Lista telefônica',
+    'mo_ModuleModulePhoneBook' => 'Módulo de lista telefônica',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Módulo da lista telefônica -% repesent%',
+];
