54b5a1478cdd8f7e71dffcbb8b2fe170e5b1fc39
Jochem Pluim <jochem@cafedoranjeboom.nl>
2023-07-01 23:20:34 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    'module_phnbk_Disconnected' => 'Module uitgeschakeld',
+    'module_phnbk_Connected' => 'Module ingeschakeld',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'Het nummer %repesent% bestaat al',
+    'module_phnbk_AddNewRecord' => 'Nieuw record toevoegen',
+    'module_phnbk_ColumnNumber' => 'Telefoonnummer',
+    'module_phnbk_ColumnName' => 'Naam contact',
+    'SubHeaderModulePhoneBook' => 'PBX gebruikt de records als Caller-ID en toont deze op het display van het toestel',
+    'BreadcrumbModulePhoneBook' => 'Telefoonboek',
+    'mo_ModuleModulePhoneBook' => 'Telefoonboek',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Telefoonboek - %repesent%',
+];
