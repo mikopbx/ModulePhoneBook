d6a96b7e02f05b963718dea5814c09e3779f9630
Anonymous <noreply@weblate.org>
2023-09-01 12:01:47 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/ro/

--- Messages/ro.php
+++ Messages/ro.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Agenda telefonică a modulului - %repesent%',
+    'mo_ModuleModulePhoneBook' => 'Modul agenda telefonica',
+    'SubHeaderModulePhoneBook' => 'Vă permite să afișați numele abonaților în timpul unui apel și în istoricul conversațiilor',
+    'module_phnbk_ColumnName' => 'Numele abonatului',
+    'module_phnbk_ColumnNumber' => 'Telefon',
+    'module_phnbk_AddNewRecord' => 'Adăugați o intrare nouă',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'Numărul de intrare %repesent% există deja',
+    'module_phnbk_Connected' => 'Modul conectat',
+    'BreadcrumbModulePhoneBook' => 'Carte de telefoane',
+    'module_phnbk_Disconnected' => 'Modul dezactivat',
+];
