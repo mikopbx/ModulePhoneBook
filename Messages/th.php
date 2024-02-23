dc86a60da09128d5bdd689d943d04070ee100844
Anonymous <noreply@weblate.org>
2024-02-23 02:57:40 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'โมดูลสมุดโทรศัพท์ - % ซ้ำ%',
+    'mo_ModuleModulePhoneBook' => 'โมดูลสมุดโทรศัพท์',
+    'BreadcrumbModulePhoneBook' => 'สมุดโทรศัพท์',
+    'SubHeaderModulePhoneBook' => 'ช่วยให้คุณแสดงชื่อผู้โทรระหว่างการโทรและในประวัติการสนทนา',
+    'module_phnbk_ColumnName' => 'ชื่อสมาชิก',
+    'module_phnbk_ColumnNumber' => 'โทรศัพท์',
+    'module_phnbk_AddNewRecord' => 'เพิ่มรายการใหม่',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'หมายเลขบันทึก %repesent% มีอยู่แล้ว',
+    'module_phnbk_Connected' => 'เชื่อมต่อโมดูลแล้ว',
+    'module_phnbk_Disconnected' => 'ปิดใช้งานโมดูลแล้ว',
+];
