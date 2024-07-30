ca15a6d31ae26395c2f98a845fcff77a7f933cfd
Anonymous <noreply@weblate.org>
2024-07-30 07:24:00 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Telefonkönyv modul – %repesent%',
+    'mo_ModuleModulePhoneBook' => 'Telefonkönyv modul',
+    'BreadcrumbModulePhoneBook' => 'Telefonkönyv',
+    'SubHeaderModulePhoneBook' => 'Lehetővé teszi a hívók nevének megjelenítését hívás közben és a beszélgetési előzményekben',
+    'module_phnbk_ColumnName' => 'Előfizető neve',
+    'module_phnbk_ColumnNumber' => 'telefon',
+    'module_phnbk_AddNewRecord' => 'Új bejegyzés hozzáadása',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'A(z) %repesent% rekordszám már létezik',
+    'module_phnbk_Connected' => 'Modul csatlakoztatva',
+    'module_phnbk_Disconnected' => 'Modul letiltva',
+];
