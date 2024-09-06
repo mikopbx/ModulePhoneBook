6c14ea3843810d7bfc73ae04f98ff461015767a3
Anonymous <noreply@weblate.org>
2024-09-06 04:00:48 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePhoneBook' => 'Puhelinmuistiomoduuli - %repesent%',
+    'mo_ModuleModulePhoneBook' => 'Puhelinmuistio moduuli',
+    'BreadcrumbModulePhoneBook' => 'Puhelinluettelo',
+    'SubHeaderModulePhoneBook' => 'Voit näyttää soittajien nimet puhelun aikana ja keskusteluhistoriassa',
+    'module_phnbk_ColumnName' => 'Tilaajan nimi',
+    'module_phnbk_ColumnNumber' => 'Puhelin',
+    'module_phnbk_AddNewRecord' => 'Lisää uusi merkintä',
+    'module_phnbk_AlreadyExistWithThisNumber' => 'Tietuenumero %repesent% on jo olemassa',
+    'module_phnbk_Connected' => 'Moduuli kytketty',
+    'module_phnbk_Disconnected' => 'Moduuli poistettu käytöstä',
+];
