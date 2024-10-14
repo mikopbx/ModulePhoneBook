d9da42ab356f02251bafb8f3e8b28318641d41c7
Anonymous <noreply@weblate.org>
2024-10-14 10:53:23 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/pl/

--- Messages/pl.php
+++ Messages/pl.php
@@ -17,4 +17,19 @@ return [
  *
  */
     'repModulePhoneBook' => 'Moduł książki telefonicznej -% repesent%',
+    'module_phnbk_Search' => 'Znajdować...',
+    'module_phnbk_SettingsTab' => 'Ustawienia',
+    'module_phnbk_disableInputMask' => 'Wyłącz maskę liczbową',
+    'module_phnbk_ImportFromExcel' => 'Import',
+    'module_phnbk_ImportExcelTab' => 'Importuj z Excela',
+    'module_phnbk_PhonebookTab' => 'Książka telefoniczna',
+    'module_phnbk_ImportFromExcelLabel' => 'Wybierz plik Excel do pobrania',
+    'module_phnbk_ExcelInstructionHeader' => 'Instrukcja pobierania książki telefonicznej z programu Excel',
+    'module_phnbk_ExcelInstructionStep1' => 'Plik musi być w formacie <strong>.xls</strong> lub <strong>.xlsx.</strong>',
+    'module_phnbk_ExcelInstructionStep2' => 'Plik musi zawierać dwie kolumny:',
+    'module_phnbk_ExcelInstructionStep2_1' => '<strong>IDCallerID</strong> - nazwa abonenta (na przykład: Iwan Iwanow).',
+    'module_phnbk_ExcelInstructionStep2_2' => '<strong>Numer telefonu</strong> - w formacie <em>+7(926)123-45-67</em>.',
+    'module_phnbk_ExcelInstructionStep3' => 'Każda linia reprezentuje wpis książki telefonicznej.',
+    'module_phnbk_ExcelInstructionStep4' => 'Przed przesłaniem upewnij się, że dane w pliku są prawidłowe.',
+    'module_phnbk_ImportError' => 'Błąd podczas zapisywania wpisu',
 ];
