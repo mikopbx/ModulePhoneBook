80fe072a043f9c6a50480c17467f6551162a8cc5
Anonymous <noreply@weblate.org>
2024-10-14 10:53:05 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/en/

--- Messages/en.php
+++ Messages/en.php
@@ -1,21 +1,35 @@
 <?php
-/**
+return [
+    /**
  * Copyright (C) MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Nikolay Beketov, 6 2018
  *
  */
-
-return [
-    'repModulePhoneBook'                      => 'Module phonebook - %repesent%',
-    'mo_ModuleModulePhoneBook'                => 'Module phonebook',
-    'BreadcrumbModulePhoneBook'               => 'Phonebook',
-    'SubHeaderModulePhoneBook'                => 'PBX uses the records as caller id, and shows it on ip-phones display',
-    'module_phnbk_ColumnName'                 => 'Caller ID',
-    'module_phnbk_ColumnNumber'               => 'Phone number',
-    'module_phnbk_AddNewRecord'               => 'Add new records',
+    'repModulePhoneBook' => 'Module phonebook - %repesent%',
+    'mo_ModuleModulePhoneBook' => 'Module phonebook',
+    'BreadcrumbModulePhoneBook' => 'Phonebook',
+    'SubHeaderModulePhoneBook' => 'PBX uses the records as caller id, and shows it on ip-phones display',
+    'module_phnbk_ColumnName' => 'Caller ID',
+    'module_phnbk_ColumnNumber' => 'Phone number',
+    'module_phnbk_AddNewRecord' => 'Add new records',
     'module_phnbk_AlreadyExistWithThisNumber' => 'The number %repesent% already exists',
-    'module_phnbk_Connected'                  => 'Module enabled',
-    'module_phnbk_Disconnected'               => 'Module disabled',
+    'module_phnbk_Connected' => 'Module enabled',
+    'module_phnbk_Disconnected' => 'Module disabled',
+    'module_phnbk_Search' => 'Find...',
+    'module_phnbk_PhonebookTab' => 'Phone book',
+    'module_phnbk_ExcelInstructionStep2_2' => '<strong>Phone number</strong> — in the format <em>+7(926)123-45-67</em>.',
+    'module_phnbk_ExcelInstructionStep3' => 'Each line represents a phone book entry.',
+    'module_phnbk_ImportError' => 'Error saving record',
+    'module_phnbk_ImportFromExcel' => 'Import',
+    'module_phnbk_ImportExcelTab' => 'Import from Excel',
+    'module_phnbk_ImportFromExcelLabel' => 'Select Excel file to upload',
+    'module_phnbk_ExcelInstructionHeader' => 'Instructions for downloading a phone book from Excel',
+    'module_phnbk_ExcelInstructionStep1' => 'The file must be in <strong>.xls</strong> or <strong>.xlsx.</strong> format.',
+    'module_phnbk_ExcelInstructionStep2' => 'The file must contain two columns:',
+    'module_phnbk_ExcelInstructionStep2_1' => '<strong>CallerID</strong> — subscriber name (for example: Ivan Ivanov).',
+    'module_phnbk_ExcelInstructionStep4' => 'Please make sure the data in the file is correct before uploading it.',
+    'module_phnbk_SettingsTab' => 'Settings',
+    'module_phnbk_disableInputMask' => 'Disable mask for numbers',
 ];
