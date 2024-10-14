384a6c90413a8a8b952218edc2fd095294b7c924
Anonymous <noreply@weblate.org>
2024-10-14 10:53:43 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: MIKOPBX/ModulePhoneBook
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulephonebook/es/

--- Messages/es.php
+++ Messages/es.php
@@ -17,4 +17,19 @@ return [
  *
  */
     'repModulePhoneBook' => 'Módulo de agenda telefónica -% repesent%',
+    'module_phnbk_Search' => 'Encontrar...',
+    'module_phnbk_ImportFromExcel' => 'Importar',
+    'module_phnbk_SettingsTab' => 'Ajustes',
+    'module_phnbk_disableInputMask' => 'Desactivar máscara numérica',
+    'module_phnbk_ImportExcelTab' => 'Importar desde Excel',
+    'module_phnbk_PhonebookTab' => 'Guía telefónica',
+    'module_phnbk_ImportFromExcelLabel' => 'Seleccione el archivo Excel para descargar',
+    'module_phnbk_ExcelInstructionHeader' => 'Instrucciones para descargar una guía telefónica desde Excel',
+    'module_phnbk_ExcelInstructionStep1' => 'El archivo debe tener el formato <strong>.xls</strong> o <strong>.xlsx.</strong>',
+    'module_phnbk_ExcelInstructionStep2' => 'El archivo debe contener dos columnas:',
+    'module_phnbk_ExcelInstructionStep2_1' => '<strong>Identificador de llamadas</strong> - nombre del suscriptor (por ejemplo: Ivan Ivanov).',
+    'module_phnbk_ExcelInstructionStep2_2' => '<strong>Número de teléfono</strong> - en el formato <em>+7(926)123-45-67</em>.',
+    'module_phnbk_ExcelInstructionStep3' => 'Cada línea representa una entrada de la guía telefónica.',
+    'module_phnbk_ExcelInstructionStep4' => 'Asegúrese de que los datos del archivo sean correctos antes de cargarlo.',
+    'module_phnbk_ImportError' => 'Error al guardar la entrada',
 ];
