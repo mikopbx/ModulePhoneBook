a7106980f6df207c7130f5af710c20601ff9683a
Дмитрий Трацевский <frantsuzzz.2008@gmail.com>
2025-10-06 16:51:34 +0300

    Основной функционал - поиск имени по Api, если нет в справочнике

--- Lib/PhoneBookAgi.php
+++ Lib/PhoneBookAgi.php
@@ -23,6 +23,7 @@ namespace Modules\ModulePhoneBook\Lib;
 use MikoPBX\Core\Asterisk\AGI;
 use MikoPBX\Core\System\Util;
 use Modules\ModulePhoneBook\Models\PhoneBook;
+use Modules\ModulePhoneBook\Models\Settings;
 use Phalcon\Di\Injectable;
 
 /**
@@ -48,12 +49,17 @@ class PhoneBookAgi extends Injectable
             } else {
                 $number = $agi->request['agi_extension'];
             }
-
+            $number_orig = $number;
             // Normalize the phone number to match the expected format (last 9 digits)
             $number = '1' . substr($number, -9);
 
             // Find the corresponding phonebook entry by the number
             $result = PhoneBook::findFirstByNumber($number);
+
+            if (!($result !== NULL && !empty($result->call_id))){
+                // The record was not found - we are searching through the API
+                $result = self::findApiByNumber($number, $number_orig);
+            }
 
             // If a matching record is found and the call_id is not empty, set the appropriate caller ID
             if ($result !== null && !empty($result->call_id)) {
@@ -67,5 +73,63 @@ class PhoneBookAgi extends Injectable
             // Log the error message if an exception occurs
             Util::sysLogMsg('PhoneBookAGI', $e->getMessage(), LOG_ERR);
         }
+    }
+
+    /**
+     * Find CallerID from API
+     *
+     * @param string $number
+     * @param string|null $number_orig
+     * @return object|null
+     */
+    private static function findApiByNumber(string $number, ?string $number_orig = null): ?object
+    {
+        $return = null;
+        if (!empty($number)) {
+            $settings = Settings::findFirst();
+            $url = !empty($settings->phoneBookApiUrl) ? str_replace(
+                '%number%',
+                $number,
+                $settings->phoneBookApiUrl
+            ) : null;
+
+            if (!empty($url)) {
+                $return = self::curl_get_contents($url);
+
+                // Logging
+                Util::sysLogMsg(
+                    'PhoneBookAGI',
+                    'Find CallerID from API: ' . $number . ' => ' . (empty($return) ? 'NOT FOUND' : $return),
+                    LOG_INFO
+                );
+
+                if (!empty($return)) {
+                    // Saving the number in the phonebook
+                    $importer = new PhoneBookImport();
+                    $number = '1' . substr($importer->cleanPhoneNumber($number), -9);
+                    $numberRep = empty($number_orig) ? substr($number, -9) : $importer->cleanPhoneNumber($number_orig);
+                    $importer->savePhonebookRecord(trim($return), $numberRep, $number, $_SERVER['REQUEST_TIME']);
+                }
+            }
+        }
+
+        return empty($return) ? null : (object)['call_id' => trim($return), 'number' => $number];
+    }
+
+    /**
+     * Get the $url content with CURL
+     *
+     * @param string $url
+     * @return string|null
+     */
+    private static function curl_get_contents(string $url): ?string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3); // Short timeout - 3 sec
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return empty(trim($result)) ? null : trim($result);
     }
 }
